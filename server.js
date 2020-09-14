5a74c5fa960a76cd03716934ab93af7fe6ceedf8
Perry Theobald <perrytjr@gmail.com>
2020-09-14 08:28:49 -0700

    starter file for api routes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var express = require("express");
+const express = require("express");
 
 
 
-var app = express();
+const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+app.use(express.static("db"));
 
 // ROUTER
 
