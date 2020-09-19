f30e2e1a73e01567550561e0698ad12f1faff778
Perry Theobald <perrytjr@gmail.com>
2020-09-19 09:42:34 -0700

    json works

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 https://heynode.com/tutorial/readwrite-json-files-nodejs
 
 https://www.geeksforgeeks.org/node-js-fs-writefilesync-method/?ref=lbp
+
+https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+
+https://www.geeksforgeeks.org/node-js-fs-writefilesync-method/?ref=lbp
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
+
+https://www.codementor.io/@codementorteam/how-to-use-json-files-in-node-js-85hndqt32
+
+https://www.codementor.io/@codementorteam/how-to-use-json-files-in-node-js-85hndqt32
+
+https://expressjs.com/en/api.html
+
+https://masteringjs.io/tutorials/express/json
+
+https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
+
+https://www.rithmschool.com/courses/node-express-fundamentals/json-with-express
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,32 @@
 
-const fs = require("fs");
-const path = require("path");
+const getData = require("../db/db.json");
 
 
 
 
 module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
+    
   
     app.get("/api/notes", function(req, res) {
-      res.json(tableData);
+      res.json(getData);
     });
   
- 
+ let id = 0;
   
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
+
   
     app.post("/api/notes", function(req, res) {
-      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-      // It will do this by sending out the value "true" have a table
-      // req.body is available since we're using the body parsing middleware
-      if (tableData.length < 5) {
-        tableData.push(req.body);
-        res.json(true);
-      }
-      else {
-        waitListData.push(req.body);
-        res.json(false);
-      }
+      req.body.id = ++id;
+      getData.push(req.body);
+      res.json(req.body);
+    
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+      const deleteId = req.params.id;
+      const idIndex = getData.findIndex((data) => data.id == deleteId);
+      getData.splice(idIndex, 1);
+      res.json({ok: true});
     });
   
   };
